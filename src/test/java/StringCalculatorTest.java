ef57496899625e833080db07780fbfe4a3a50da9
AndrewMiddletonBC <andrew.middleton@bellevuecollege.edu>
2021-06-23 12:25:40 -0700

    Test class initialization
    Wrote initial failing tests

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
-
+	
+	@Test
+	void negativeNumberThrowsTest() {
+		assertThrows(Exception.class, () -> StringCalculator.add("-1,5"));
+	}
+	
+	@Test
+	void numberOverThousandIgnored() {
+		assertEquals(1, StringCalculator.add("1,1000"));
+	}
 }
